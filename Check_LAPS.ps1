d52e88ea7b820670da37958a86359ecc5cd0bfbd
wetcoriginal <82039108+wetcoriginal@users.noreply.github.com>
2022-11-04 11:15:12 +0100

    Update Check_LAPS.ps1

--- Check_LAPS.ps1
+++ Check_LAPS.ps1
@@ -16,15 +16,15 @@ param
 )
  
 #Liste des PC présent dans l'OU :
-$Compte=Get-ADComputer -Filter {(enabled -eq $TRUE)} -SearchBase 'OU=Ordinateurs,DC=exo,DC=com' -SearchScope Subtree | Foreach-Object Name
+$Compte=Get-ADComputer -Filter {(enabled -eq $TRUE)} -SearchBase 'OU=Ordinateurs,DC=domain,DC=lan' -SearchScope Subtree | Foreach-Object Name
 #Count du nombre de PC présent dans l'OU :
-$AllComputersCount=(Get-ADComputer -Filter 'enabled -eq $TRUE' -SearchBase 'OU=Ordinateurs,DC=exo,DC=com').Count
+$AllComputersCount=(Get-ADComputer -Filter 'enabled -eq $TRUE' -SearchBase 'OU=Ordinateurs,DC=domain,DC=lan').Count
 #Nombre de PC présent dans l'OU ayant LAPS
-$YesLAPS=(Get-ADComputer -Filter 'enabled -eq $TRUE' -Properties Name,ms-Mcs-AdmPwdExpirationTime -SearchBase 'OU=Ordinateurs,DC=exo,DC=com' | Where-Object ms-Mcs-AdmPwdExpirationTime -ne $null).count
+$YesLAPS=(Get-ADComputer -Filter 'enabled -eq $TRUE' -Properties Name,ms-Mcs-AdmPwdExpirationTime -SearchBase 'OU=Ordinateurs,DC=domain,DC=lan' | Where-Object ms-Mcs-AdmPwdExpirationTime -ne $null).count
 #Nombre de PC n'ayant pas LAPS
-$NoLAPSCount=(Get-ADComputer -Filter 'enabled -eq $TRUE' -Properties Name,ms-Mcs-AdmPwdExpirationTime -SearchBase 'OU=Ordinateurs,DC=exo,DC=com' | Where-Object ms-Mcs-AdmPwdExpirationTime -eq $null).count
+$NoLAPSCount=(Get-ADComputer -Filter 'enabled -eq $TRUE' -Properties Name,ms-Mcs-AdmPwdExpirationTime -SearchBase 'OU=Ordinateurs,DC=domain,DC=lan' | Where-Object ms-Mcs-AdmPwdExpirationTime -eq $null).count
 #Liste des PC présent dans l'OU n'ayant pas LAPS
-$NoLAPS=Get-ADComputer -Filter 'enabled -eq $TRUE' -Properties Name,ms-Mcs-AdmPwdExpirationTime -SearchBase 'OU=Ordinateurs,DC=exo,DC=com' | Where-Object ms-Mcs-AdmPwdExpirationTime -eq $null| Format-Table Name | out-string
+$NoLAPS=Get-ADComputer -Filter 'enabled -eq $TRUE' -Properties Name,ms-Mcs-AdmPwdExpirationTime -SearchBase 'OU=Ordinateurs,DC=domain,DC=lan' | Where-Object ms-Mcs-AdmPwdExpirationTime -eq $null| Format-Table Name | out-string
  
 # check that powershell ActiveDirectory module is present
 if(Get-Module -Name "ActiveDirectory" -ListAvailable)
